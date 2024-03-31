76bb5b95bb13ed41f19a9e32594ad856364d4005
Manas Srivastava <rockingmanas78@gmail.com>
2024-03-31 12:10:52 +0530

    user mail rejected!

--- app/api/booking/route.ts
+++ app/api/booking/route.ts
@@ -87,7 +87,7 @@ export async function POST(req: NextRequest, res: NextResponse) {
       });
     });
     await sendMailPromise(mailOptions);
-    await sendMailPromise(userMailOptions);
+    //await sendMailPromise(userMailOptions);
 
     return NextResponse.json({ message: 'Booking Successfully Created!' });
   } catch (error) {
