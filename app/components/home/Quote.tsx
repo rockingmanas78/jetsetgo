f2e37a47b81c7d8831edeb3498e87e7da06764fc
Manas Srivastava <rockingmanas78@gmail.com>
2024-08-31 12:05:58 +0530

    quotes color changed

--- app/components/home/Quote.tsx
+++ app/components/home/Quote.tsx
@@ -9,7 +9,7 @@ interface QuoteProps {
 
 const Quote: FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center py-12 my-8 bg-gradient-to-r from-[#0c1f2f] via- via-[#193855] via-[#AFC1D0] via-[#003D60] to-[#0c1f2f] text-white px-4 md:px-8">{/**/}
+    <div className="flex flex-col items-center justify-center py-12 my-8 bg-gradient-to-r from-[#0c1f2f] via- via-[#193855] via-[#8799A7] via-[#003D60] to-[#0c1f2f] text-white px-4 md:px-8">{/**/}
       <p className="text-base sm:text-lg md:text-xl lg:text-2xl font-thin italic leading-relaxed max-w-4xl text-center">
         "{message}"
       </p>
