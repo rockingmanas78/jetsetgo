b970a714c431cb38867b2e55ff9dca83e0c70594
Manas Srivastava <rockingmanas78@gmail.com>
2024-08-31 11:58:34 +0530

    Revert "bug fixes and ui updates"
    This reverts commit b20699e353afae4d49c78b77e1c4117ac7221f68.

--- app/api/booking/route.ts
+++ app/api/booking/route.ts
@@ -48,6 +48,9 @@ export async function POST(req: NextRequest, res: NextResponse) {
             </div>
         </div>
     `;
+
+    console.log(process.env.NEXT_PUBLIC_MAIL_USERNAME);
+    console.log(process.env.NEXT_PUBLIC_MAIL_PASSWORD);
     // Create transporter for sending emails
     const transporter = nodemailer.createTransport({
       host: "smtp.privateemail.com",
